8f7480cc8ac3348300aa2ddea41893019a4ee14b
Edumug <eduardoarnhold301@gmail.com>
2025-08-27 16:27:00 -0300

    Update Menu.java

--- FinalProject-main/FinalProject/src/finalproject/Menu.java
+++ FinalProject-main/FinalProject/src/finalproject/Menu.java
@@ -34,7 +34,7 @@ public class Menu extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Menu");
+        setTitle("Menu Erick is my Hero");
 
         jPanel1.setBackground(new java.awt.Color(153, 153, 255));
 
